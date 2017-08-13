7a7af6df05460e0080ab96cb67b7954a2709bbef
Zia <zia_gt@yahoo.com>
2017-08-14 01:27:37 +0200

    categories js added

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,50 @@
-$(function(){
-    
-});
+var moviesApplication = {};
+moviesApplication.database = [];
+
+moviesApplication.init = function(){
+    moviesApplication.filterSlider();
+    moviesApplication.getTypes();
+};
+
+moviesApplication.loadAssets = function(){
+	$.getJSON('resources/database/movies.json', function(data){
+		moviesApplication.database = data;
+		moviesApplication.init();
+	});
+};
+
+moviesApplication.filterSlider = function(){
+    $('.filter.open').on('click', function(){
+        $('.filter_container').slideToggle(300, function(){
+            var button = $(this).prev();
+
+            if(button.hasClass('active')){
+                $('.filter.open').find('.btn_title').text('Filter By');
+                button.removeClass('active');
+            }else {
+                $('.filter.open').find('.btn_title').text('Close');
+                button.addClass('active');
+            }
+        });
+    });
+};
+
+moviesApplication.getTypes = function(){
+
+    var types = [];
+
+    $.each(moviesApplication.database, function(index, element){
+        if($.inArray(moviesApplication.database[index].type, types)){
+            var typeValue = moviesApplication.database[index].type;
+        types.push(typeValue);
+        $('#categories').append('<option value="'+typeValue+'">'+typeValue+'</option>');
+        }
+        
+
+        
+    });
+};
+
+
+
+moviesApplication.loadAssets();
